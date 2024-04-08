ffc875dce61c13ec6e068c2e355b9241996b159d
Muin-Uddin-Ahmad <muinuddin.sm12@gamil.com>
2024-04-08 16:05:37 +0600

    Footer section added

--- src/components/Estates.jsx
+++ src/components/Estates.jsx
@@ -1,24 +1,23 @@
 import { useLoaderData } from "react-router-dom";
 import Estate from "./Estate";
 
-
 const Estates = () => {
-    const estates = useLoaderData();
-    // console.log(estateData)
-    return (
-        <section className="max-w-[1440px] py-10 mx-auto px-4 md:px-[135px] sm:py-12 mb-10">
+  const estates = useLoaderData();
+  // console.log(estateData)
+  return (
+    <section className="max-w-[1440px] py-10 mx-auto px-4 md:px-[135px] sm:py-12 mb-6">
       <div className="container mx-auto space-y-8">
         <div className="space-y-2 text-center">
-          <h2 className="text-3xl font-bold">Estate Section</h2>
+          <h2 className="text-[28px] font-semibold">Estate Section</h2>
         </div>
         <div className="grid grid-cols-1  gap-x-4 gap-y-4 md:grid-cols-2 lg:grid-cols-3">
-          {
-            estates.map((estate)=> <Estate key={estate.id} estate={estate}></Estate>)
-          }
+          {estates.map((estate) => (
+            <Estate key={estate.id} estate={estate}></Estate>
+          ))}
         </div>
       </div>
     </section>
-    );
+  );
 };
 
 export default Estates;
